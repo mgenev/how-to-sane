bd26d21895cf6c222f411beb035a7c7a2170ba6a
Martin Genev <mgenev@gmail.com>
2014-12-17 15:35:13 -0800

    Update route.js

--- client/app/pods/posts/create/route.js
+++ client/app/pods/posts/create/route.js
@@ -6,7 +6,6 @@ export default Ember.Route.extend({
     },
     actions: {
         createPost: function(model) {
-            console.log('model', model);
             var _this = this;
             model.save().then(post => _this.transitionTo('posts.post', post));
         }
