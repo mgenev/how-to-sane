305032a421d46073d4b140465a325c456b2a2dcf
Martin Genev <mgenev@gmail.com>
2014-12-18 11:03:17 -0800

    added template model

---
+++ server/api/models/Template.js
@@ -0,0 +1,18 @@
+/**
+ * Template.js
+ *
+ * @description :: TODO: You might write a short summary of how this model works and what it represents here.
+ * @docs        :: http://sailsjs.org/#!documentation/models
+ */
+
+module.exports = {
+
+    attributes: {
+        title: {
+            type: 'string'
+        },
+        filePath: {
+            type: 'string'
+        }
+    }
+};
