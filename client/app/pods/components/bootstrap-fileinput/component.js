c3ae24a42da1940614e8bb6684def606d1b51de5
Martin Genev <mgenev@gmail.com>
2014-12-26 20:26:14 -0800

    improved file input logic for dynamic urls

--- client/app/pods/components/bootstrap-fileinput/component.js
+++ client/app/pods/components/bootstrap-fileinput/component.js
@@ -3,7 +3,7 @@ import Ember from 'ember';
 export default Ember.Component.extend({
 	didInsertElement: function () {
 		$("#input-id").fileinput({
-			uploadUrl: "/api/v1/uploads/upload",
+			uploadUrl: this.get('url'),
 			multiple: true
 		});
 	}
