944f61bf3705a0dc0650ffe61390919a7f1f2638
Martin Genev <mgenev@gmail.com>
2014-12-26 22:11:56 -0800

    uploader now opens in modal

--- client/app/pods/components/modal-dialog/component.js
+++ client/app/pods/components/modal-dialog/component.js
@@ -1,15 +1,15 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-	action: "closeModal",
+    action: "closeModal",
     didInsertElement: function() {
-    	var self = this;
+        var self = this;
         var modal = $('#modalDialog').modal({
             'show': true
         });
         // TODO: Check to see if we need to cleanup this on event upon destroy
         modal.on('hidden.bs.modal', function() {
-        	self.sendAction();
+            self.sendAction();
         });
     },
     willDestroyElement: function() {
