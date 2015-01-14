cbeef2b4cbb7cb2ed07f9eec31891c551e82879a
Martin Genev <mgenev@gmail.com>
2015-01-13 20:44:33 -0800

    added a status card component for the feed

--- client/app/pods/s/feed/index/route.js
+++ client/app/pods/s/feed/index/route.js
@@ -2,6 +2,6 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
 	model: function () {
-		return this.store.find('status');
+		return this.store.find('status', {sort: "createdAt desc"});
 	}
 });
