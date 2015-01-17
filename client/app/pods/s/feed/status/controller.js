9a3b9efae2065475b0f552dee65c0df9b243dc65
Martin Genev <mgenev@gmail.com>
2015-01-16 23:08:03 -0800

    adjusted statuses to the changed note attribute

--- client/app/pods/s/feed/status/controller.js
+++ client/app/pods/s/feed/status/controller.js
@@ -3,7 +3,7 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
     steps: [{
         caption: 'Post your message',
-        component: 'status-post'
+        component: 'status-note'
     }, {
         caption: 'Post what you are doing',
         component: 'status-activity'
