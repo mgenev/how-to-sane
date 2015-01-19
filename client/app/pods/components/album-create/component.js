0e265eec256fa5ab4279b4e5755f888c435ca570
Martin Genev <mgenev@gmail.com>
2015-01-18 20:10:16 -0800

    gave create album the modal look, added wookmark

---
+++ client/app/pods/components/album-create/component.js
@@ -0,0 +1,19 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+	
+	createModel: function () {
+		 this.store.createRecord('album').then(model => this.set('albumModel', model));
+	}.on('didInsertElement'),
+
+	actions: {
+        createAlbum: function(model) {        	
+        	var userId = this.session.get('user.id');
+
+            this.store.find('user', userId).then(function(result) {
+                model.set('user', result);
+                return model.save();
+            }).then(() => this.transitionTo('s.media.upload'));
+        }
+    }
+});
