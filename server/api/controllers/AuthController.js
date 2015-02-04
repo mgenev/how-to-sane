d068e75d2e430c2214df99c130078c2b7f962004
Martin Genev <mgenev@gmail.com>
2015-02-03 19:46:09 -0800

    tokens now refresh and the api doesn't deny you

--- server/api/controllers/AuthController.js
+++ server/api/controllers/AuthController.js
@@ -1,11 +1,14 @@
 var passport = require('passport');
 var jwt = require('jsonwebtoken');
+var _ = require('lodash');
 var secret = 'RS#$09qu43f09qfj94qf*&H#(R';
+var refreshSecret = 'rw5&&$$2224124f*&H#(R';
+
 /**
  * AuthController
  *
  * @module      :: Controller
- * @description	:: A set of functions called `actions`.
+ * @description :: A set of functions called `actions`.
  *
  *                 Actions contain code telling Sails how to respond to a certain type of request.
  *                 (i.e. do stuff, then send some JSON, show an HTML page, or redirect to another URL)
@@ -20,26 +23,58 @@ var secret = 'RS#$09qu43f09qfj94qf*&H#(R';
 
 module.exports = {
     login: function(req, res) {
-        passport.authenticate('local', {session: false}, function(err, user, info) {
-            if ((err) || (!user)) {
-                res.badRequest({
-                    error: 'invalidPassword'                    
-                });
-                return;
-            } else {
-                if (err) {
+
+        if (req.body.grant_type === 'password') {
+
+            passport.authenticate('local', {
+                session: false
+            }, function(err, user, info) {
+                if ((err) || (!user)) {
                     res.badRequest({
-                        error: 'unknownError: ' + err
+                        error: 'invalidPassword'
                     });
+                    return;
                 } else {
-                    var token = jwt.sign(user, secret, { expiresInMinutes: 60*24 });
-                    res.send({
-                        user: user,
-                        access_token: token
-                    });
+                    if (err) {
+                        res.badRequest({
+                            error: 'unknownError: ' + err
+                        });
+                    } else {
+                        issueTokens(user, res);
+                    }
+                }
+            })(req, res);
+
+        } else if (req.body.grant_type === 'refresh_token' && req.body.refresh_token) {
+
+            var token, user;
+
+            if (req.headers && req.headers.authorization) {
+                var parts = req.headers.authorization.split(' ');
+                if (parts.length == 2) {
+                    var scheme = parts[0],
+                        credentials = parts[1];
+
+                    if (/^Bearer$/i.test(scheme)) {
+                        token = credentials;
+                    }
+                } else {
+
                 }
             }
-        })(req, res);
+            var bearerToken, refreshToken;
+
+            bearerToken = jwt.verify(token, secret);
+            refreshToken = jwt.verify(req.body.refresh_token, refreshSecret);
+
+            if (_.isEqual(bearerToken, refreshToken)) {
+                delete bearerToken.exp;
+                delete bearerToken.iat;
+
+                user = bearerToken;
+                issueTokens(user, res);
+            };
+        }
     },
 
     logout: function(req, res) {
@@ -50,3 +85,22 @@ module.exports = {
         });
     }
 };
+
+function issueTokens(user, res) {
+    var expirationTimeInMinutes = 60 * 2;
+
+    var token = jwt.sign(user, secret, {
+        expiresInMinutes: expirationTimeInMinutes
+    });
+
+    var refreshToken = jwt.sign(user, refreshSecret, {
+        expiresInMinutes: expirationTimeInMinutes
+    });
+
+    res.send({
+        user: user,
+        access_token: token,
+        expires_in: expirationTimeInMinutes * 60, // because simple auth expects seconds
+        refresh_token: refreshToken
+    });
+}
