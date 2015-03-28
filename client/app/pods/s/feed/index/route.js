b2f597e50142a408c01805f0a9def6d7517d09a9
Martin Genev <mgenev@gmail.com>
2015-03-28 15:13:52 -0700

    added a search by date example for feed

--- client/app/pods/s/feed/index/route.js
+++ client/app/pods/s/feed/index/route.js
@@ -1,7 +1,18 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-	model: function () {
-		return this.store.find('status', {sort: 'createdAt desc'});
-	}
+	queryParams: {
+		fromDate: {
+			refreshModel: true
+		}
+	},
+	fromDate: null,
+    model: function (params) {
+        return this.store.find('status', {
+            sort: 'createdAt desc',
+            createdAt: {
+                ">": params.fromDate
+            }
+        });
+    }
 });
