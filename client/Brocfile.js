e413c5666cb0c11f0e235b72610d1ad7fdb2463e
Martin Genev <mgenev@gmail.com>
2015-04-03 13:51:41 -0700

    added yeti through bower and broccoli

--- client/Brocfile.js
+++ client/Brocfile.js
@@ -5,7 +5,7 @@ var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 var app = new EmberApp();
 
 app.import('bower_components/bootstrap/dist/js/bootstrap.min.js');
-// app.import('bower_components/bootstrap/dist/css/bootstrap.min.css');
+app.import('bower_components/bsw-yeti/css/bootstrap.min.css');
 
 app.import('vendor/bootstrap-fileinput/fileinput.js');
 app.import('bower_components/bootstrap-fileinput/css/fileinput.min.css');
