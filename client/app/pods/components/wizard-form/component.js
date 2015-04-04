92e6bf1fd610ac9b7fd50df9024b2e03725f84dc
feitian124 <feitian124@gmail.com>
2015-04-04 12:57:09 +0800

    fix wizard-form component test

--- client/app/pods/components/wizard-form/component.js
+++ client/app/pods/components/wizard-form/component.js
@@ -1,56 +1,57 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-    activeStep: 0,
+  activeStep: 0,
 
-    type: Ember.computed('activeStep', function () {
-        return this.get('steps')[this.get('activeStep')].component;
-    }),
+  type: Ember.computed('activeStep', function () {
+    var steps = this.get('steps');
+    return steps[this.get('activeStep')].component;
+  }),
 
-    firstStep: Ember.computed('activeStep', function () {
-        return (this.get('activeStep') === 0);
-    }),
+  firstStep: Ember.computed('activeStep', function () {
+    return (this.get('activeStep') === 0);
+  }),
 
-    lastStep: Ember.computed('activeStep', function () {
-        return (this.get('activeStep') === this.get('steps').length - 1);
-    }),
+  lastStep: Ember.computed('activeStep', function () {
+    return (this.get('activeStep') === this.get('steps').length - 1);
+  }),
 
-    actions: {
-        nextStep() {
-                this.incrementProperty('activeStep');
-                this.addActiveClass();
-            },
+  addActiveClass() {
+    $('.wizard-active').removeClass('wizard-active');
 
-            prevStep() {
-                this.decrementProperty('activeStep');
-                this.addActiveClass();
-            },
+    let selector = '.wizard-step:eq(' + this.get('activeStep') + ')';
+    $(selector).addClass('wizard-active');
+  },
 
-            goToStep(step) {
-                this.set('activeStep', step);
-                this.addActiveClass();
-            },
+  didInsertElement() {
+    this.addActiveClass();
+  },
 
-            submitAction() {
-                if (this.get('lastStep')) {
-                    this.sendAction('action', this.get('status'));
-                } else {
-                    this.triggerAction({
-                        action: 'nextStep',
-                        target: this
-                    });
-                }
-            }
+  actions: {
+    nextStep() {
+      this.incrementProperty('activeStep');
+      this.addActiveClass();
     },
 
-    addActiveClass() {
-        $('.wizard-active').removeClass('wizard-active');
-
-        let selector = '.wizard-step:eq(' + this.get('activeStep') + ')';
-        $(selector).addClass('wizard-active');
+    prevStep() {
+      this.decrementProperty('activeStep');
+      this.addActiveClass();
     },
 
-    didInsertElement() {
-        this.addActiveClass();
+    goToStep(step) {
+      this.set('activeStep', step);
+      this.addActiveClass();
+    },
+
+    submitAction() {
+      if (this.get('lastStep')) {
+        this.sendAction('action', this.get('status'));
+      } else {
+        this.triggerAction({
+          action: 'nextStep',
+          target: this
+        });
+      }
     }
+  }
 });
