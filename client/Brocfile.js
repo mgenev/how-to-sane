90d8f60c69195bd597d92dff02db7228573cb52c
Martin Genev <mgenev@gmail.com>
2015-04-04 20:05:04 -0700

    switched to plain bootstrap, bower's yeti version broke icons

--- client/Brocfile.js
+++ client/Brocfile.js
@@ -5,7 +5,6 @@ var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 var app = new EmberApp();
 
 app.import('bower_components/bootstrap/dist/js/bootstrap.min.js');
-// app.import('bower_components/bsw-yeti/css/bootstrap.min.css');
 app.import('bower_components/bootstrap/dist/css/bootstrap.min.css');
 
 
