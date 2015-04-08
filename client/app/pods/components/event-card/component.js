057c075e5e77bcda037a3116f2613ebfcce07013
Martin Genev <mgenev@gmail.com>
2015-04-08 15:36:29 -0700

    improvement to events

--- client/app/pods/components/event-card/component.js
+++ client/app/pods/components/event-card/component.js
@@ -1,10 +1,15 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-
+    store: Ember.inject.service(),
     actions: {
         joinEvent(event) {
-            this.sendAction('joinEvent', event);
+            // this.sendAction('joinEvent', event);
+            let userId = this.session.get('user.id');
+            this.get('store').findById('user', userId).then(user => {
+                event.get('users').addObject(user);
+                event.save();
+            });
         }
     }
 });
