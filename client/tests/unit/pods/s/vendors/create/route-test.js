cf5bfc5f714763a65731e8d794427bda42fd10b7
mgenev <mgenev@gmail.com>
2015-04-12 00:20:15 -0700

    added missing vendors route tests

---
+++ client/tests/unit/pods/s/vendors/create/route-test.js
@@ -0,0 +1,11 @@
+import { expect } from 'chai';
+import { describeModule, it } from 'ember-mocha';
+
+describeModule('route:s/vendors/create', 'SVendorsCreateRoute', {
+    // needs: ['controller:foo']
+}, function () {
+    it('exists', function () {
+        var route = this.subject();
+        expect(route).to.be.ok;
+    });
+});
