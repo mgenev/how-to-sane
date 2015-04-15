f33632b6bfe362dfd6a73d2e7d8626f4257d1464
Martin Genev <mgenev@gmail.com>
2015-04-15 11:16:20 -0700

    2 space indent formatting

--- client/app/pods/s/page-manager/route.js
+++ client/app/pods/s/page-manager/route.js
@@ -2,30 +2,30 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
 
-    actions: {
-        cancel() {
-          this.transitionTo('s.page-manager');
-        },
-        update(model) {
-          return model.save().then(
-              savedModel => {
-                  console.log('page ' + savedModel.get('name') + ' saved successfully');
-                  this.transitionTo('s.page-manager');
-              },
-              reason => {
-                  console.log('error saving page, reason: ' + reason);
-                  this.transitionTo('s.page-manager');
-              }
-          );
-        },
-        delete(model) {
-          return model.destroyRecord().then(
-              () => this.transitionTo('s.page-manager'),
-              reason => {
-                  console.log('error deleting page, reason was: ' + reason);
-                  this.transitionTo('s.page-manager');
-              }
-          );
-        }
+  actions: {
+    cancel() {
+      this.transitionTo('s.page-manager');
+    },
+    update(model) {
+      return model.save().then(
+          savedModel => {
+            console.log('page ' + savedModel.get('name') + ' saved successfully');
+            this.transitionTo('s.page-manager');
+          },
+          reason => {
+            console.log('error saving page, reason: ' + reason);
+            this.transitionTo('s.page-manager');
+          }
+      );
+    },
+    delete(model) {
+      return model.destroyRecord().then(
+          () => this.transitionTo('s.page-manager'),
+          reason => {
+            console.log('error deleting page, reason was: ' + reason);
+            this.transitionTo('s.page-manager');
+          }
+      );
     }
+  }
 });
