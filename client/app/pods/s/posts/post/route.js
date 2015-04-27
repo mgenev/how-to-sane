f1cd6584ffcebecda1c72e1918407d52ad3caae9
Martin Genev <mgenev@gmail.com>
2015-04-27 13:04:30 -0700

    async await with ajax

--- client/app/pods/s/posts/post/route.js
+++ client/app/pods/s/posts/post/route.js
@@ -4,30 +4,20 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model: function (params) {
 
-		function sleep(milliseconds) {
-		  var start = new Date().getTime();
-		  for (var i = 0; i < 1e7; i++) {
-		    if ((new Date().getTime() - start) > milliseconds){
-		      break;
-		    }
-		  }
-		}
-
     async function bar() {
       console.log('TEST 2. bar is running now');
-			sleep(3000);
-      if (true) {
-        return 'TEST 3. i am the value of bar';
-      } else {
-        throw new Error('TEST Error!');
+        return Ember.$.ajax({
+							url: '/api/v2/users',
+							type: 'GET',
+							contentType: 'application/json'
+					});
       }
-    }
 
     async function foo() {
       console.log('TEST 1. i totally work');
 			try {
 				let result = await bar();
-				console.log(result);
+				console.log('TEST ajax result', result);
 				console.log('TEST 4. now I ran');
 	    } catch (err) {
 	      console.log('TEST THE ERROR: ', err);
