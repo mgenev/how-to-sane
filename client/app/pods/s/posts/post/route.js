3581f79f9dbd29308981ba1eb00a6b7c011dc0e0
Martin Genev <mgenev@gmail.com>
2015-04-27 14:40:34 -0700

    arrived to a pattern for requesting ajax and populating route models

--- client/app/pods/s/posts/post/route.js
+++ client/app/pods/s/posts/post/route.js
@@ -2,23 +2,23 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  model: function (params) {
-
-    async function foo() {
+  model(params) {
+    let foo = async () => {
 			try {
-				let result = await Ember.$.ajax({
-							url: '/api/v1/users',
-							type: 'GET'
-					});
-				console.log('TEST', result);
+				let result =  await Ember.$.ajax({
+  				url: '/api/v1/posts/' + params.post_id,
+  				type: 'GET'
+				});
+
+        this.store.pushPayload(result);
+        return this.store.all('post').content[0];
+
 	    } catch (err) {
 	      console.log('TEST THE ERROR: ', err);
 	    }
     }
 
-		foo();
-
-    return this.store.find('post', params.post_id);
+    return foo();
   }
 });
 /* jshint ignore:end */
