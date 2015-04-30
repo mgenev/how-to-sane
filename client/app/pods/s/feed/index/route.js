dd37c665294d3c01ae3ca7053a5c2aa2c393f5da
Martin Genev <mgenev@gmail.com>
2015-04-29 20:57:55 -0700

    fixing bugs in geo with es7

--- client/app/pods/s/feed/index/route.js
+++ client/app/pods/s/feed/index/route.js
@@ -1,17 +1,29 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-	queryParams: {
-		fromDate: {
-			refreshModel: true
+  queryParams: {
+    fromDate: {
+      refreshModel: true
+    }
+  },
+	async setupController(controller, model) {
+		try {
+			let currentGeo = await this.geoGoogleService.getGeoposition();
+			let geo = this.geoGoogleService.getGoogleMapsGeoCoords(currentGeo);
+			let currentAddress = await this.geoGoogleService.getAddressForLatLong(geo);
+			controller.set('geo', currentGeo);
+			controller.set('address', currentAddress);
+		}
+		catch (err) {
+			console.log('GEO ERROR', err);
 		}
 	},
-    model: function (params) {
-        return this.store.find('status', {
-            sort: 'createdAt desc',
-            createdAt: {
-                ">": params.fromDate
-            }
-        });
-    }
+  model: function (params) {
+    return this.store.find('status', {
+      sort: 'createdAt desc',
+      createdAt: {
+        ">": params.fromDate
+      }
+    });
+  }
 });
