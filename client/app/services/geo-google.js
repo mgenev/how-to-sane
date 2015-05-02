472cdfb74f482770c92a19d2513b2e64607feaf0
mgenev <mgenev@gmail.com>
2015-05-02 16:34:21 -0700

    geo service syntax to es7

--- client/app/services/geo-google.js
+++ client/app/services/geo-google.js
@@ -1,14 +1,11 @@
 /* jshint ignore:start */
 import Ember from 'ember';
-import {
-  point
-}
-from 'client/utils/to-geo-json';
+import { point } from 'client/utils/to-geo-json';
 
 export default Ember.Object.extend({
 
-  getGeoposition: function () {
-
+  getGeoposition() {
+    // TODO try async here
     return new Promise( (resolve, reject) => {
       let success = pos => resolve(pos.coords);
       let error = err => console.warn(`ERROR(${err.code}): ${err.message}`);
@@ -16,70 +13,62 @@ export default Ember.Object.extend({
     });
   },
 
-  getGoogleMapsGeoCoords: function (geo) {
+  getGoogleMapsGeoCoords(geo) {
     return new google.maps.LatLng(geo.latitude, geo.longitude);
   },
 
-  geocoding: function (lookup, reverse) {
-    var url;
+  geocoding(lookup, reverse) {
+    let url;
     if (reverse) {
       url = 'https://maps.googleapis.com/maps/api/geocode/json?latlng=' + lookup.A + ',' + lookup.F;
     } else {
       url = 'https://maps.googleapis.com/maps/api/geocode/json?address=' + lookup;
     }
-
     return Ember.$.ajax({
       url: url,
       type: 'POST'
     });
   },
-
-  getCurrentAddress: function () {
+  getCurrentAddress() {
     return this.getAddressForLatLong(this.getGeoposition());
   },
 
-  getLatLongForAddress: async function (address) {
+  async getLatLongForAddress(address) {
     let location = await this.geocoding(address);
     return point(location.results[0].geometry.location);
   },
 
-  getAddressForLatLong: async function (latlong) {
-    return this.geocoding(latlong, true).then(function (result) {
-      return result.results[0].formatted_address;
-    });
+  async getAddressForLatLong(latlong) {
+    let geo = await this.geocoding(latlong, true);
+    return geo.results[0].formatted_address;
   },
 
-  getNearbyPlaces: function (geo, pinMarkers) {
-    var _this = this;
-
-    var request = {
-      location: this.googleMapsGeoCoords(geo),
+  getNearbyPlaces(geo, pinMarkers) {
+    let request = {
+      location: this.getGoogleMapsGeoCoords(geo),
       radius: '500',
       query: 'restaurant'
     };
 
-    var service = new google.maps.places.PlacesService(this.get('map'));
+    // TODO rewrite with async
+    let service = new google.maps.places.PlacesService(this.get('map'));
     service.textSearch(request, callback);
 
-    function callback(results, status) {
-
-      _this.set('nearbyLocations', results);
-
-      // if response and boolean present - pin the markers
+    let callback = (results, status) => {
+      this.set('nearbyPlaces', results);
       if (status === google.maps.places.PlacesServiceStatus.OK && pinMarkers) {
         for (var i = 0; i < results.length; i++) {
-          _this.createMarker(results[i]);
+          this.createMarker(results[i]);
         }
       }
-
     }
   },
 
-  drawMap: function (geo, mapElementSelector) {
+  drawMap(geo, mapElementSelector, pinCenter) {
 
-    var center = this.getGoogleMapsGeoCoords(geo);
-    var infoWindow = new google.maps.InfoWindow();
-    var map = new google.maps.Map(document.getElementById(mapElementSelector), {
+    let center = this.getGoogleMapsGeoCoords(geo);
+    let infoWindow = new google.maps.InfoWindow();
+    let map = new google.maps.Map(document.getElementById(mapElementSelector), {
       center: center,
       zoom: 14
     });
@@ -87,24 +76,24 @@ export default Ember.Object.extend({
     this.set('infoWindow', infoWindow);
     this.set('map', map);
 
-    new google.maps.Marker({
-      map: map,
-      position: center,
-      icon: '/images/icons/smiley_happy.png'
-    });
+    if (pinCenter) {
+      new google.maps.Marker({
+        map: map,
+        position: center,
+        icon: '/images/icons/smiley_happy.png'
+      });
+    }
   },
 
-  createMarker: function (place) {
-
-    var location = {
+  createMarker(place) {
+    let location = this.getGoogleMapsGeoCoords({
       latitude: parseFloat(place.coordinates[1]),
       longitude: parseFloat(place.coordinates[0])
-    };
-    var placeLoc = this.getGoogleMapsGeoCoords(location);
+    });
 
-    var marker = new google.maps.Marker({
+    let marker = new google.maps.Marker({
       map: this.get('map'),
-      position: placeLoc
+      position: location
     });
 
     google.maps.event.addListener(marker, 'click', () => {
