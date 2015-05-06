de4d2dae347d3216f26e71ab4cb976fa9d67c41f
Ian VanSchooten <ian@badgelabsllc.com>
2015-05-06 01:27:36 -0400

    Add upload button and action
    Needed to manually include the authorization token, because ember-simple-auth
    was not automatically injecting it in the upload request sent by ember-plupload

--- client/app/pods/s/media/upload/route.js
+++ client/app/pods/s/media/upload/route.js
@@ -9,32 +9,45 @@ export default Ember.Route.extend({
     });
   },
 
-  actions: {
-    queuePhoto: function queuePhoto(file) {
-      // let album = this.modelFor(this.routeName);
-      // if (album.get('photos') == null) {
-      //   album.set('photos', Ember.A([]));
-      // }
+  setupController: function (controller, album) {
+    let accessToken = this.container.lookup('simple-auth-authorizer:oauth2-bearer').session.content.access_token;
+    let headers = {
+      Authorization: 'bearer ' + accessToken
+    };
+    controller.set('requestHeaders', headers);
+    controller.set('model', album);
+  },
 
+  actions: {
+    queuePhoto(file) {
+      // Initialize a 'photos' array property on the controller
       let controller = this.controller;
       if (controller.get('photos') == null) {
         controller.set('photos', Ember.A([]));
       }
 
-      file.read().then
-      // The following creates a data URI which can be used in the
-      // app as an img src, for instance.  It then adds the current
-      // file to the controller array of photos
+      // The following is for ember-plupload. It creates a data URI
+      // which can be used in the app as an img src, for instance.
+      // It then adds the current file to the controller array of photos
       file.read().then(function setPreviewUrl(dataURI) {
         file.set('preview', dataURI);
       }).then(function addFileToController(){
         controller.get('photos').pushObject(file);
       });
     },
-    save: function uploadPhotos() {
 
+    save(selectedAlbum) {
+      let controller = this.controller;
+      let photos = controller.get('photos');
+      if (photos) {
+        photos.forEach(function(photo) {
+          photo.uploader.settings.multipart_params = {'albumId': selectedAlbum.id};
+          photo.upload();
+        });
+      }
     },
-    delete: function removePhoto(photoFile) {
+
+    delete(photoFile) {
       let controller = this.controller;
       let filteredArray = controller.get('photos').rejectBy('file', photoFile.file);
       controller.set('photos', filteredArray);
