7170dedff3d6cfe64b2cba96036c914661e9e0bd
Martin Genev <mgenev@gmail.com>
2015-05-08 13:54:51 -0700

    successfully using es7 decorators for computed

--- client/app/models/user.js
+++ client/app/models/user.js
@@ -1,27 +1,27 @@
 import DS from 'ember-data';
 import Ember from 'ember';
-
-const { computed } = Ember;
+import computed from 'ember-computed-decorators';
 
 export default DS.Model.extend({
 
-    //relationships
-    vendors: DS.hasMany('vendor'),
-    photos: DS.hasMany('photo'),
-    albums: DS.hasMany('album'),
-    posts: DS.hasMany('post'),
-    statuses: DS.hasMany('status'),
-    events: DS.hasMany('event'),
+  //relationships
+  vendors: DS.hasMany('vendor'),
+  photos: DS.hasMany('photo'),
+  albums: DS.hasMany('album'),
+  posts: DS.hasMany('post'),
+  statuses: DS.hasMany('status'),
+  events: DS.hasMany('event'),
 
-    // attributes
-    firstName: DS.attr('string'),
-    lastName: DS.attr('string'),
-    tagline: DS.attr('string'),
-    email: DS.attr('string'),
-    website: DS.attr('string'),
+  // attributes
+  firstName: DS.attr('string'),
+  lastName: DS.attr('string'),
+  tagline: DS.attr('string'),
+  email: DS.attr('string'),
+  website: DS.attr('string'),
 
-    // computed
-    fullName: computed('firstName', 'lastName', function () {
-        return this.get('firstName') + ' ' + this.get('lastName');
-    })
+  // computed
+  @computed('firstName', 'lastName')
+  fullName(f, l) {
+    return `${f} ${l}`;
+  }
 });
