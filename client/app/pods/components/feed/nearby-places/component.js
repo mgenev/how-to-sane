81a9251e922722641e6227b3dc7405ccb2a65ab4
Ian VanSchooten <ian@badgelabsllc.com>
2015-06-24 23:53:25 -0400

    Revert b09690a

--- client/app/pods/components/feed/nearby-places/component.js
+++ client/app/pods/components/feed/nearby-places/component.js
@@ -1,7 +1,4 @@
 import Ember from 'ember';
-import Paginate from 'client/mixins/generic-paginator';
 
-export default Ember.Component.extend(Paginate, {
-  pageSize: 5,
-  listToPaginate: Ember.computed.alias('nearbyPlaces')
+export default Ember.Component.extend({
 });
